f012dad19aa3376363a8e5525b838b61bcc4b0e1
Shane Sveller <shanesveller@gmail.com>
2018-10-21 10:06:51 -0500

    Install distillery

--- mix.exs
+++ mix.exs
@@ -22,6 +22,8 @@ defmodule KubeNative.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:distillery, "~> 2.0"}
+    ]
   end
 end
