3211f65f6e18d4e5b38b128bdd3db5e6a5aa9749
Shane Sveller <shanesveller@gmail.com>
2018-11-12 18:11:29 -0600

    Add Phoenix hostname, port and secret key base to runtime config

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,1 +1,8 @@
 use Mix.Config
+
+port = String.to_integer(System.get_env("PORT") || "4000")
+
+config :kube_native_web, KubeNativeWeb.Endpoint,
+  http: [port: port],
+  url: [host: System.get_env("HOSTNAME"), port: port],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
